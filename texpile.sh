f7d230c6fbaa609c0316a7c2a80c6f6fc32630e3
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2023-01-23 21:08:23 +0000

    Added slides for the FCC workshop in Krakow

--- texpile.sh
+++ texpile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #f=ARC_Presentation_ECFA_Workshop_DESY_6th_October_2022
-f=ARC_Presentation_CEPC_Workshop_27th_October_2022
+#f=ARC_Presentation_CEPC_Workshop_27th_October_2022
+#f=ARC_Presentation_DD4HEPIntro_15th_December_2022
+f=ARC_Presentation_FCC_Workshop_Krakow_25th_January_2023
 if pdflatex $f.tex
 then
   pdflatex $f.tex
