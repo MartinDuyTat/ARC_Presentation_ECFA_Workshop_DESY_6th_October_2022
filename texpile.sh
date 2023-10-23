c250135b0d6df51971d42553547564fccecd0efb
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2023-10-23 20:27:01 +0100

    2

--- texpile.sh
+++ texpile.sh
@@ -2,7 +2,8 @@
 #f=ARC_Presentation_ECFA_Workshop_DESY_6th_October_2022
 #f=ARC_Presentation_CEPC_Workshop_27th_October_2022
 #f=ARC_Presentation_DD4HEPIntro_15th_December_2022
-f=ARC_Presentation_FCC_Workshop_Krakow_25th_January_2023
+#f=ARC_Presentation_FCC_Workshop_Krakow_25th_January_2023
+f=ARC_Presentation_FCC_ImplementationDiscussion_24th_October_2023
 if pdflatex $f.tex
 then
   pdflatex $f.tex
